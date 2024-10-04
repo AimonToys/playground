12f0bea6ccda51ee54aea82e3b9c4e707c39e467
Dmitry Matasov <matasoff@aimon.toys>
2024-10-04 19:05:53 -0400

    audio-only

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -1,8 +1,9 @@
 import { useConnectionState, useLocalParticipant, useTracks, useVoiceAssistant } from "@livekit/components-react";
 import { ConnectionState, LocalParticipant, Track } from "livekit-client";
 import { useEffect, useMemo } from "react";
-import { VideoTrack } from "@livekit/components-react";
+import { VideoTrack, BarVisualizer } from "@livekit/components-react";
 import { useConfig } from "@/hooks/useConfig";
+import { LoadingSVG } from "@/components/button/LoadingSVG";
 
 export interface PlaygroundProps {
   onConnect: (connect: boolean) => void;
@@ -53,6 +54,50 @@ export default function Playground({ onConnect }: PlaygroundProps) {
     );
   }, [agentVideoTrack, roomState]);
 
+
+  const audioTileContent = useMemo(() => {
+    const disconnectedContent = (
+      <div className="flex flex-col items-center justify-center gap-2 text-gray-700 text-center w-full">
+        No audio track. Connect to get started.
+      </div>
+    );
+
+    const waitingContent = (
+      <div className="flex flex-col items-center gap-2 text-gray-700 text-center w-full">
+        <LoadingSVG />
+        Waiting for audio track
+      </div>
+    );
+
+    const visualizerContent = (
+      <div
+        className={`flex items-center justify-center w-full h-48 [--lk-va-bar-width:30px] [--lk-va-bar-gap:20px] [--lk-fg:var(--lk-theme-color)]`}
+      >
+        <BarVisualizer
+          state={voiceAssistant.state}
+          trackRef={voiceAssistant.audioTrack}
+          barCount={5}
+          options={{ minHeight: 20 }}
+        />
+      </div>
+    );
+
+    if (roomState === ConnectionState.Disconnected) {
+      return disconnectedContent;
+    }
+
+    if (!voiceAssistant.audioTrack) {
+      return waitingContent;
+    }
+
+    return visualizerContent;
+  }, [
+    voiceAssistant.audioTrack,
+    config.settings.theme_color,
+    roomState,
+    voiceAssistant.state,
+  ]);
+
   return (
     <div className="flex flex-col items-center gap-4">
       <button
@@ -65,8 +110,8 @@ export default function Playground({ onConnect }: PlaygroundProps) {
       >
         {roomState === ConnectionState.Connected ? "Disconnect" : "Connect"}
       </button>
-      <div className="w-[360px] h-[360px] bg-gray-800 rounded-lg overflow-hidden">
-        {videoContent}
+      <div className="w-[360px] h-[360px] bg-gray-800 rounded-lg overflow-hidden flex items-center justify-center">
+        {audioTileContent}
       </div>
     </div>
   );
